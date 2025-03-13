2fc503bc180b53adf0a0cd834ceff4dcb52a5a8b
helvetios16 <smendozafernandez437@gmail.com>
2025-03-13 11:30:50 -0500

    feat: play and pause continue music (one)

---
+++ src/components/PlayerMusic.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-row justify-between w-full px-4 z-50">
+    <div>Current song ...</div>
+    <div class="grid place-content-center gap-4 flex-1">
+      <div class="flex justify-center">
+        <button class="bg-white rounded-full p-2" @click="tooglePlay">
+          <component :is="play ? PauseIcon : PlayIcon"></component>
+        </button>
+      </div>
+    </div>
+    <div>Volumen</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PauseIcon from '@/assets/icons/PauseIcon.vue';
+import PlayIcon from '@/assets/icons/PlayIcon.vue';
+import { ref } from 'vue';
+
+const play = ref(false);
+const audio = new Audio();
+
+audio.src = '/music/1/01.mp3';
+
+const tooglePlay = () => {
+  if (play.value) {
+    audio.pause();
+  } else {
+    audio.volume = 0.5;
+    audio.play();
+  }
+  play.value = !play.value;
+};
+
+// se puede desmontar para que cuando se vaya un lado pare la música pero no es necesario ya que
+// tiene que persistir en toda la aplicación
+</script>
